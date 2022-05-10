c158594a8eed18c3cc8cda8ebfa8bdb72411a161
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2022-05-10 11:45:40 +0530

    adds basics folder

---
+++ src/ts-basics/index.ts
@@ -0,0 +1,10 @@
+const a = "Hello";
+const b = "Byebye";
+
+const add = () => {
+  return a + b;
+};
+// This is a comment
+
+console.log(add());
+export { add };
---
+++ src/ts-basics/out/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = void 0;
+const a = "Hello";
+const b = "Byebye";
+const add = () => {
+  return a + b;
+};
+exports.add = add;
+console.log(add());
